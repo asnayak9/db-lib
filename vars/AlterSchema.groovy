cd683ae870e567fd7bc741dd94f5ca2fd714211a
mkbcode <monoj.bisoyi@gmail.com>
2023-05-22 22:59:51 +0530

    test jenkins shared library

---
+++ vars/AlterSchema.groovy
@@ -0,0 +1,19 @@
+import groovy.sql.Sql
+import groovy.text.SimpleTemplateEngine
+
+def prepareAlterScript() {
+    def changeRequestContent = "ALTER TABLE `master_db`.`sy_parameter` \n" +
+            "ADD COLUMN `sy_parametercol` VARCHAR(45) NULL AFTER `DB_Version`"
+    def dbUrl = 'jdbc:mysql://localhost:3306/master_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC'
+    //DriverManager.registerDriver(new com.mysql.jdbc.Driver())
+    sql = Sql.newInstance(dbUrl, 'root', 'Mamata@143123', 'com.mysql.jdbc.Driver')
+    def row = sql.firstRow("SELECT DB_Version as oldV FROM master_db.sy_parameter")
+    def oldV = row.getProperty("oldV") as BigDecimal
+    sql.close()
+    def binding = [oldV: oldV, newV: oldV = 0.001, changeRequestContent: changeRequestContent]
+
+    def template = new SimpleTemplateEngine().createTemplate(new File('C:\\Users\\91789\\git\\db-processor\\vars\\alter_schema_template.sql').text)
+
+    def result = template.make(binding).toString()
+    println result
+}
---
+++ vars/alter_schema_template.sql
@@ -0,0 +1,10 @@
+--test template
+define old_rev=${oldV}
+define new_rev=${newV}
+
+--actual script start
+
+${changeRequestContent}
+
+--actual script end
+commit;
