52c2cf040391a50d51f504b2da19257e4b6bdc8d
mkbcode <monoj.bisoyi@gmail.com>
2023-05-23 09:56:26 +0530

    sample file added in vars

--- src/org/foo/AlterSchema.groovy
+++ src/org/foo/AlterSchema.groovy
@@ -4,6 +4,10 @@ package org.foo
 import groovy.sql.Sql
 import groovy.text.SimpleTemplateEngine
 
+@Grapes(
+        @Grab(group='com.mysql', module='mysql-connector-j', version='8.0.33')
+)
+
 class AlterSchema {
   def prepareAlterScript() {
     def changeRequestContent = "ALTER TABLE `master_db`.`sy_parameter` \n" +
