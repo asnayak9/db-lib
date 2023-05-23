0768a5add7651875e90def3ebf3a04915d34f5b0
mkbcode <monoj.bisoyi@gmail.com>
2023-05-23 10:21:36 +0530

    sample file added in vars

--- src/org/foo/AlterSchema.groovy
+++ src/org/foo/AlterSchema.groovy
@@ -4,7 +4,6 @@ package org.foo
 import groovy.sql.Sql
 import groovy.text.SimpleTemplateEngine
 
-@GrabConfig( systemClassLoader=true )
 @Grapes(
         @Grab(group='com.mysql', module='mysql-connector-j', version='8.0.33')
 )
@@ -14,12 +13,12 @@ class AlterSchema {
     def changeRequestContent = "ALTER TABLE `master_db`.`sy_parameter` \n" +
             "ADD COLUMN `sy_parametercol` VARCHAR(45) NULL AFTER `DB_Version`"
     def dbUrl = 'jdbc:mysql://localhost:3306/master_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC'
-    //DriverManager.registerDriver(new com.mysql.jdbc.Driver())
-    sql = Sql.newInstance(dbUrl, 'root', 'Mamata@143123', 'com.mysql.jdbc.Driver')
+    DriverManager.registerDriver(new com.mysql.jdbc.Driver())
+    sql = Sql.newInstance(dbUrl, 'root', 'Mamata@143123')
     def row = sql.firstRow("SELECT DB_Version as oldV FROM master_db.sy_parameter")
     def oldV = row.getProperty("oldV") as BigDecimal
     sql.close()
-    def binding = [oldV: oldV, newV: oldV = 0.001, changeRequestContent: changeRequestContent]
+    def binding = [oldV: oldV, newV: oldV += 0.001, changeRequestContent: changeRequestContent]
 
     def template = new SimpleTemplateEngine().createTemplate(new File('C:\\Users\\91789\\git\\db-processor\\vars\\alter_schema_template.sql').text)
 
