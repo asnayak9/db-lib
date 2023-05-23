4f8668d3b42fb7a3fe52d9cabbb59832666f71c1
mkbcode <monoj.bisoyi@gmail.com>
2023-05-23 15:50:43 +0530

    file upload added

--- src/org/foo/AlterSchema.groovy
+++ src/org/foo/AlterSchema.groovy
@@ -28,5 +28,22 @@ class AlterSchema {
     println result
     return result
   }
+
+  def inputGetFile() {
+    def filedata = null
+    def filename = null
+    // Get file using input step, will put it in build directory
+    // the filename will not be included in the upload data, so optionally allow it to be specified
+
+    def inputFile = input message: 'Upload file', parameters: [file(name: 'library_data_upload')]
+    filedata = inputFile
+    filename = savedfile
+
+    // Read contents and write to workspace
+    writeFile(file: filename, encoding: 'Base64', text: filedata.read().getBytes().encodeBase64().toString())
+    // Remove the file from the master to avoid stuff like secret leakage
+    //filedata.delete()
+    return filename
+  }
 }
 
