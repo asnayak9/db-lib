c951d25cdeb1d879ca8da83f64b5c352c477ca4a
mkbcode <monoj.bisoyi@gmail.com>
2023-05-23 10:29:32 +0530

    sample file added in vars

--- src/org/foo/AlterSchema.groovy
+++ src/org/foo/AlterSchema.groovy
@@ -6,7 +6,9 @@ import groovy.sql.DriverManager
 import groovy.text.SimpleTemplateEngine
 
 @Grapes(
-        @Grab(group='com.mysql', module='mysql-connector-j', version='8.0.33')
+        @Grab(group='com.mysql', module='mysql-connector-j', version='8.0.33'),
+        @Grab(group='org.apache.groovy', module='groovy-sql', version='4.0.2'),
+        @Grab(group='org.apache.groovy', module='groovy-templates', version='4.0.2')
 )
 
 class AlterSchema {
